3cabc6ae1a07444d1c8468863406f0bbd63632e2
puppyapple <puppyapplewzj@126.com>
2018-05-19 16:12:10 +0800

    sf

--- Code/bids_infos_extraction.py
+++ Code/bids_infos_extraction.py
@@ -1,20 +1,37 @@
+# -*- coding: utf-8 -*- 
 #%%
 import sys 
-sys.path.append("D:\\标签图谱\\标签关系\\bids_infos_extraction\\Code\\")
+# sys.path.append("D:\\标签图谱\\标签关系\\bids_infos_extraction\\Code\\")
 import os
 import pandas as pd
 import numpy as np 
 import operator
+import re
+from imp import reload
 from functools import reduce
-from utils import *
+import utils
+reload(utils)
 
+# spider.zhongbiao
+# 中标公告id: announcement_url_id
+# 中标金额：total_amount
+#%%
 key_word_dict = {
-    "bid_amount": ["中标金额", "总报价", "中标价", "中标价格", "成交价", "成交价格", "价格"],
-    "bid_winner": ["中标公司", "中标单位", "中标供应商", "成交供应商", "谈判成交供应商", "无中标公司信息", 
-        "中标人名称", "成交人", "成交人名称", "中标候选人", "成交候选供应商"]
+    "bid_winner": {
+        "step": 3,
+        "key_word_list": ["中标公司", "中标单位", "中标供应商", "成交供应商", "谈判成交供应商", "无中标公司信息", 
+        "中标人名称", "成交人", "成交人名称", "中标候选人", "成交候选供应商"],
+        "regex_list": [".+公司"]
+        },
+    "bid_amount": {
+        "step": 1, 
+        "key_word_list": ["中标金额", "总报价", "中标价", "中标价格", "成交价", "成交价格", "价格", "成交金额"],
+        "regex_list": [".*"]
+        }    
 }
 
-a = pd.read_html('../Data/Input/bids_infos_extraction/test2.html', encoding='utf8')
-a[0]
+
+html = '../Data/Input/bids_infos_extraction/test2.html'
 #%%
-match_result("中标人名称", 3, a[0])
+# utils.text_info_finder(html, key_word_dict)
+utils.table_info_finder(html, key_word_dict)
--- Code/utils.py
+++ Code/utils.py
@@ -4,12 +4,13 @@ import os
 import pandas as pd
 import numpy as np 
 import operator
+import re
 from functools import reduce
-# spider.zhongbiao
-# 中标时间: date_award 
-# 中标金额：total_amount
 
 #%%
+def is_match(word, regex_list):
+    return sum([True for regex in regex_list if re.match(regex, word)])
+
 def search_around(coord, step, df):
     right_limit = df.shape[1]
     bottom_limit = df.shape[0]
@@ -19,23 +20,28 @@ def search_around(coord, step, df):
     values_around = [df.values[coo] for coo in coords_around]
     return values_around
 
-def match_result(key_word, step, df):
-    indexes = np.where(df == key_word)
-    coordinates = list(zip(indexes[0], indexes[1]))
+def match_result(key_word_list, step, regex_list, df):
+    indexes = [np.where(df == key_word) for key_word in key_word_list]
+    coordinates = set(reduce(lambda a, b: a + b, [list(zip(ind[0], ind[1])) for ind in indexes]))
     # print(coordinates)
-    result = reduce(lambda a,b: a + b, [search_around(coo, step, df) for coo in coordinates])
-    return result
-
-def amount_in_table(df, word_dict):
-    return
+    if len(coordinates) != 0:
+        result_raw = reduce(lambda a, b: a + b, [search_around(coo, step, df) for coo in coordinates])
+        return [r for r in result_raw if is_match(r, regex_list)]
+    else:
+        return []
 
 def table_info_finder(html_text, key_word_dict):
-    table_list = pd.read_html(html_text, encoding='utf8')
-    return
+    try:
+        table_list = pd.read_html(html_text, encoding='utf8')
+    except ValueError:
+        # print("No tables found!")
+        return {}
+    else:
+        result_dict = {k: ",".join(reduce(lambda a, b: a + b, [match_result(v["key_word_list"], v["step"], v["regex_list"], df) for df in table_list])) for k, v in key_word_dict.items()}
+        return result_dict
 '''
 #%%
 df = pd.DataFrame([[1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15], [16,17,18,19,20]])
-df
-#%%
+
 match_result(8, 3, df)
 '''
