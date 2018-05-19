2e86462872687032b1b9ede66ee2b99393689c9d
puppyapple <puppyapplewzj@126.com>
2018-05-19 17:59:49 +0800

    sdf

--- Code/bids_infos_extraction.py
+++ Code/bids_infos_extraction.py
@@ -13,14 +13,18 @@ import utils
 reload(utils)
 
 # spider.zhongbiao
-# 中标公告id: announcement_url_id
-# 中标金额：total_amount
+# 中标公告id: announcement_url_id(无缺失)
+# 采购单位： caigou_unit(无缺失)
+# 中标金额：total_amount(部分缺失)
+# 中标单位：bid_winner(文本+table)
+# 中标单位角色：匹配到的关键字
 #%%
 key_word_dict = {
     "bid_winner": {
         "step": 3,
-        "key_word_list": ["中标公司", "中标单位", "中标供应商", "成交供应商", "谈判成交供应商", "无中标公司信息", 
-        "中标人名称", "成交人", "成交人名称", "中标候选人", "成交候选供应商"],
+        "key_word_list": ["中标公司", "中标公司名称" ,"中标单位", "中标单位名称", "中标供应商", "中标供应商名称", 
+        "成交供应商", "成交供应商名称", "谈判成交供应商", "谈判成交供应商名称", "中标人", "中标人名称", 
+        "成交人", "成交人名称", "中标候选人", "中标候选人名称", "成交候选供应商", "成交候选供应商名称", "无中标公司信息"],
         "regex_list": [".+公司"]
         },
     "bid_amount": {
@@ -30,8 +34,7 @@ key_word_dict = {
         }    
 }
 
-
-html = '../Data/Input/bids_infos_extraction/test2.html'
 #%%
 # utils.text_info_finder(html, key_word_dict)
+html = '../Data/Input/bids_infos_extraction/test2.html'
 utils.table_info_finder(html, key_word_dict)
--- Code/utils.py
+++ Code/utils.py
@@ -7,10 +7,13 @@ import operator
 import re
 from functools import reduce
 
+# 文本读取
 #%%
+# 判断词是否至少命中正则规则中的一个
 def is_match(word, regex_list):
     return sum([True for regex in regex_list if re.match(regex, word)])
 
+# 返回某个值在dataframe右方和下方step个位置的值列表
 def search_around(coord, step, df):
     right_limit = df.shape[1]
     bottom_limit = df.shape[0]
@@ -20,6 +23,7 @@ def search_around(coord, step, df):
     values_around = [df.values[coo] for coo in coords_around]
     return values_around
 
+# 根据获取全部关键词列表中的词语其右方和下方的数据，经过关键词类别对应的正则规则过滤
 def match_result(key_word_list, step, regex_list, df):
     indexes = [np.where(df == key_word) for key_word in key_word_list]
     coordinates = set(reduce(lambda a, b: a + b, [list(zip(ind[0], ind[1])) for ind in indexes]))
@@ -30,6 +34,7 @@ def match_result(key_word_list, step, regex_list, df):
     else:
         return []
 
+# 根据不同的关键词（抽取主干）进行目标值提取，返回字典列表
 def table_info_finder(html_text, key_word_dict):
     try:
         table_list = pd.read_html(html_text, encoding='utf8')
@@ -39,6 +44,11 @@ def table_info_finder(html_text, key_word_dict):
     else:
         result_dict = {k: ",".join(reduce(lambda a, b: a + b, [match_result(v["key_word_list"], v["step"], v["regex_list"], df) for df in table_list])) for k, v in key_word_dict.items()}
         return result_dict
+
+# 文本提取
+def text_info_finder(html, key_word_dict):
+    return
+
 '''
 #%%
 df = pd.DataFrame([[1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15], [16,17,18,19,20]])
