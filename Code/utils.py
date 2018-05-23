c7ed9c7e53debbe91c309cc71ced29586b96b01b
puppyapple <puppyapplewzj@126.com>
2018-05-23 15:56:47 +0800

    sdf

--- Code/utils.py
+++ Code/utils.py
@@ -17,6 +17,7 @@ def is_match(word, regex_list):
 def search_around(coord, step, df):
     right_limit = df.shape[1]
     bottom_limit = df.shape[0]
+    # print(right_limit, bottom_limit)
     coords_around = [(coord[0], y) for y in range(coord[1] + 1, min(right_limit, coord[1] + step + 1))] + \
         [(x, coord[1]) for x in range(coord[0] + 1, min(bottom_limit, coord[0] + step + 1))]
     # print(coords_around)
@@ -28,12 +29,13 @@ def match_result(key_word_list, step, regex_list, df):
     # df_str = df.applymap(lambda x: str(x))
     indexes = [np.where(df.applymap(lambda x: True if re.match(key_word, str(x)) else False)) for key_word in key_word_list]
     coordinates = set(reduce(lambda a, b: a + b, [list(zip(ind[0], ind[1])) for ind in indexes]))
-    # print(coordinates)
+    # print(indexes)
     if len(coordinates) != 0:
         result_raw = reduce(lambda a, b: a.union(b), [search_around(coo, step, df) for coo in coordinates])
         # print(result_raw)
         return set([r for r in result_raw if is_match(r, regex_list)])
     else:
+        # print("empty")
         return set([])
 
 # 根据不同的关键词（抽取主干）进行目标值提取，返回字典列表
@@ -42,14 +44,19 @@ def table_info_finder(html_text, key_word_dict):
         table_list = pd.read_html(html_text, encoding='utf8')
     except ValueError:
         # print("No tables found!")
-        return {k: "" for k, v in key_word_dict.items()}
+        return []
     else:
+        tb_list = []
         for tb in table_list:       
             tb.dropna(how="all", inplace=True)
             tb.fillna("", inplace=True)
-            print(tb)
-        result_dict = {k: reduce(lambda a, b: a.union(b), [match_result(v["key_word_list"], v["step"], v["regex_list"], df)  \
-            for df in table_list]) for k, v in key_word_dict.items()}
+            tb_v = tb.values.tolist()
+            tb_v.insert(0, tb.columns)
+            tb_list.append(pd.DataFrame(tb_v))
+        # print(table_list)
+        result_dict = {k: list(reduce(lambda a, b: a.union(b), [match_result(v["key_word_list"], v["step"], v["regex_list"], df)  \
+            for df in tb_list])) for k, v in key_word_dict.items()}
+        # print(result_dict)
         if len(result_dict["bid_winner"]) == len(result_dict["bid_amount"]):
             return [(result_dict["bid_winner"][i], result_dict["bid_amount"][i]) for i in range(len(result_dict["bid_winner"]))]
         else:
@@ -58,7 +65,7 @@ def table_info_finder(html_text, key_word_dict):
 # 文本提取
 
 def text_info_finder(text, key_word_dict):
-    text_str = "".join(open(text, encoding='utf8').read().split())
+    text_str = "".join(text.split())
     result_dict_raw = {k: (v["expr"][0], re.findall(v["expr"][1], text_str)) for k, v in key_word_dict.items()}
     result_dict = {k: [r[v[0]] for r in v[1]] for k, v in result_dict_raw.items()}
     if len(result_dict["bid_winner"]) == len(result_dict["bid_amount"]):
@@ -66,3 +73,16 @@ def text_info_finder(text, key_word_dict):
     else:
         return [(result_dict["bid_winner"][i], "") for i in range(len(result_dict["bid_winner"]))]
 
+# announcement_url_id查询
+def get_bid_info(col, col_type, search_list, table_name, db_con, text_key_word_dict, table_key_word_dict):
+    sql = ""
+    if search_list == "all":
+        sql = "select id, announcement_url_id, html, ggzw_text from %s" % (table_name)
+    else:
+        condition = ",".join(map(lambda x: "'" + str(x) + "'", search_list)) if col_type == str else ",".join(map(lambda x: str(x), search_list))
+        sql = "select id, announcement_url_id, html, ggzw_text from %s where %s in (%s)" % (table_name, col, condition)
+    df = pd.read_sql(sql, con=db_con)
+    result = df[["id", "announcement_url_id"]].copy()
+    result["result_from_text"] = df["ggzw_text"].apply(lambda x: text_info_finder(x, text_key_word_dict))
+    result["result_from_table"] = df["html"].apply(lambda x: table_info_finder(x, table_key_word_dict))
+    return result
