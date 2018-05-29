c85e8129d3a28b3da58aaf74bc53c587e1999449
puppyapple <puppyapplewzj@126.com>
2018-05-29 18:22:07 +0800

    水电费

--- Code/utils.py
+++ Code/utils.py
@@ -5,7 +5,49 @@ import pandas as pd
 import numpy as np 
 import operator
 import re
+import configparser
+import pymysql
 from functools import reduce
+
+config = configparser.ConfigParser()
+config.read("../Data/Input/database_config/db_spider.conf")
+host = config['DATABASE']['host']
+user = config['DATABASE']['user']
+password = config['DATABASE']['password']
+database = config['DATABASE']['database']
+port = config['DATABASE']['port']
+charset = config['DATABASE']['charset']
+db = pymysql.connect(host=host, user=user,
+    password=password, db=database, port=int(port), charset=charset)
+
+request_key = "announcement_url_id"
+table_name = "spider.caigou_announcement_of_award"
+text_col_name="ggzw_html"
+html_col_name="ggzw_html"
+
+
+table_key_word_dict = {
+    "bid_winner": {
+        "step": 5,
+        "key_word_list": [r"^.{0,3}(中标|成交).{0,5}(人|公司|供应商|候选人|单位)(名称|信息)?$|^供应商(名称|信息)$", "第一中标候选人", "无中标公司信息"],
+        "regex_list": [r".+[公司|学校|研究所|研究院|院|所]$"]
+        },
+    "bid_amount": {
+        "step": 5, 
+        "key_word_list": [r"^.{0,3}(中标|成交|报|总报).{0,5}(价|价格|金额)"],
+        "regex_list": [r"[^\.]*[0-9]+[\.]{0,1}[0-9]+[^\.]*$", "[壹贰叁肆伍陆柒捌玖拾佰仟万亿].*"]
+        }    
+}
+
+text_key_word_dict = {
+    "bid_winner": {
+        "expr": (6, r"(((中标|成交).{0,5}(人|公司|供应商|候选人|单位)(名称|信息)?|^供应商(名称|信息))[^\u4e00-\u9fa5]{0,10}([\u4e00-\u9fa5]{1,30}(公司|学校|研究所|研究院|院|所)))")
+        },
+    "bid_amount": {
+        "expr": (3, r"(总?(中标|成交)总?报?(价格?|金额)[:|：]?(.{1,20}[元|圆][\)|\）]?))") # r"(总?(中标|成交)总?报?(价|价格|金额).[^0-9]{0,3}([0-9]+[\.]?[0-9]+.{1,30}[元|圆][\)|\）]?))"
+        }
+}
+
 
 # 文本读取
 #%%
@@ -63,7 +105,6 @@ def table_info_finder(html_text, key_word_dict):
             return [(result_dict["bid_winner"][i], "") for i in range(len(result_dict["bid_winner"]))]
 
 # 文本提取
-
 def text_info_finder(text, key_word_dict):
     text_str = "".join(text.split())
     result_dict_raw = {k: (v["expr"][0], re.findall(v["expr"][1], text_str)) for k, v in key_word_dict.items()}
@@ -74,15 +115,34 @@ def text_info_finder(text, key_word_dict):
         return [(result_dict["bid_winner"][i], "") for i in range(len(result_dict["bid_winner"]))]
 
 # announcement_url_id查询
-def get_bid_info(col, col_type, search_list, table_name, db_con, text_key_word_dict, table_key_word_dict):
+def get_bid_info(col="announcement_url_id", col_type=str, search_list="all", table_name=table_name, db_con=db, text_key_word_dict=text_key_word_dict, table_key_word_dict=table_key_word_dict):
     sql = ""
     if search_list == "all":
-        sql = "select id, announcement_url_id, url, html, ggzw_text from %s" % (table_name)
+        sql = "select * from %s" % (table_name)
     else:
         condition = ",".join(map(lambda x: "'" + str(x) + "'", search_list)) if col_type == str else ",".join(map(lambda x: str(x), search_list))
-        sql = "select id, announcement_url_id, html, ggzw_text from %s where %s in (%s)" % (table_name, col, condition)
+        sql = "select * from %s where %s in (%s)" % (table_name, col, condition)
     df = pd.read_sql(sql, con=db_con)
-    result = df[["id", "announcement_url_id", "url"]].copy()
+    result = df[["announcement_url_id", "url"]].copy()
     result["result_from_text"] = df["ggzw_text"].apply(lambda x: text_info_finder(x, text_key_word_dict))
     result["result_from_table"] = df["html"].apply(lambda x: table_info_finder(x, table_key_word_dict))
-    return result
+    result.index = result.announcement_url_id
+    result.drop(["announcement_url_id"], axis=1, inplace=True)
+    return result.to_dict(orient='index')
+
+def get_bid_info_from_table(request_key_value, request_key=request_key, table_name=table_name, text_col_name=text_col_name, html_col_name=html_col_name, 
+        db_con=db, text_key_word_dict=text_key_word_dict, table_key_word_dict=table_key_word_dict):
+    sql = "select * from %s where %s = '%s'" % (table_name, request_key, request_key_value)
+    # print(sql)
+    df = pd.read_sql(sql, con=db_con)
+    # print(df)
+    result = df[["announcement_url_id", "url"]].copy()
+    result["result_from_text"] = df[text_col_name].apply(lambda x: text_info_finder(x, text_key_word_dict))
+    result["result_from_table"] = df[html_col_name].apply(lambda x: table_info_finder(x, table_key_word_dict))
+    result.index = result.announcement_url_id
+    result.drop(["announcement_url_id"], axis=1, inplace=True)
+    return result.to_dict(orient='index')
+
+
+def get_bid_info_from_source(text, html, text_key_word_dict=text_key_word_dict, table_key_word_dict=table_key_word_dict):
+    return {"result_from_text": text_info_finder(text, text_key_word_dict), "result_from_table": table_info_finder(html, table_key_word_dict)}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # bids_infos_extraction
+01-10行：相关库import
+12-21行：读取连接库的参数生成mysql数据库连接db
+23-26行：默认的查询依据（公告id：announcement_url_id），使用的数据库表名，公告正文对应字段名，公告页html字段名（spider.caigou_announcement_of_award表中没有对公告正文进行清理提取，不存在ggzw_text字段，因此都使用了ggzw_html）
+29-49行：文本抽取和表格抽取时各自使用的正则表达式配置表，以嵌套字典储存为全局量，可进行修改、拓展
+52-81行：中间函数实现
+
+核心函数：
+84-105行：table_info_finder。从公告表格中提取信息，输入参数为html的字符串形式，和表格抽取的正则配置表。返回值为[(comp_name1, value1), (comp_name2, value2),...]的列表，comp_name为中标公司名称，value为中标金额（可能空缺）
+107-115行：text_info_finder，从公告文本中提取信息，输出参数为公告文本的字符串形式，和文本抽取的正则配置表。其余同上
+
+查询函数：
+117-131行：get_bid_info，自行批量运行测试数据时使用
+133-144行：get_bid_info_from_table，根据查询关键字（默认为announcement_url_id）在表中提取数据解析成结果，默认参数值取自上述定义的全局变量，request_key_value为announcement_url_id的字符串形式；返回结果为{announcement_url_id:{result_from_text:[(comp_name1, value1), (comp_name2, value2),...], result_from_table:[(comp_name1, value1), (comp_name2, value2),...]}}的字典形式
+147-148行：get_bid_info_from_source，根据公告文本字符串（text）和公告html字符串（html）
